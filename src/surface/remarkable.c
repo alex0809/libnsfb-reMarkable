99e22f6a97813de6c4e40c76944cd981c4f2b0f0
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-03-31 00:30:21 +0200

    Add code to refresh the screen (always full screen for now)

--- src/surface/remarkable.c
+++ src/surface/remarkable.c
@@ -22,10 +22,11 @@
 #include "nsfb.h"
 #include "surface.h"
 #include "plot.h"
-
 #include "mxcfb.h"
 
 #define UNUSED(x) ((x) = (x))
+
+int fb;
 
 static int rm_defaults(nsfb_t *nsfb)
 {
@@ -44,7 +45,7 @@ static int rm_defaults(nsfb_t *nsfb)
 
 static int rm_initialise(nsfb_t *nsfb)
 {
-    int fb = open("/dev/fb0", O_RDWR);
+    fb = open("/dev/fb0", O_RDWR);
 
     if (fb == -1) {
         fprintf(stderr, "Could not open framebuffer\n");
@@ -91,6 +92,7 @@ rm_set_geometry(nsfb_t *nsfb, int width, int height, enum nsfb_format_e format)
 static int rm_finalise(nsfb_t *nsfb)
 {
     free(nsfb->ptr);
+    close(fb);
 
     return 0;
 }
@@ -103,11 +105,39 @@ static bool rm_input(nsfb_t *nsfb, nsfb_event_t *event, int timeout)
     return false;
 }
 
+static int rm_update(nsfb_t *nsfb, nsfb_bbox_t *box) {
+    fprintf(stderr, "Surface update called with box: %d->%d %d->%d\n", box->x0, box->x1, box->y0, box->y1);
+
+    struct mxcfb_update_data update_data;
+    struct mxcfb_rect update_rect;
+    update_rect.top = box->x0;
+    update_rect.height = (box->x1 - box->x0);
+    update_rect.left = box->y0;
+    update_rect.width = (box->y1 - box->y0);
+
+    update_rect.top = 0;
+    update_rect.height = 1872;
+    update_rect.width = 1404;
+    update_rect.left = 0;
+
+    update_data.update_region = update_rect;
+    update_data.waveform_mode = 0x2;
+    update_data.update_mode = UPDATE_MODE_PARTIAL;
+    update_data.update_marker = 0;
+    update_data.dither_mode = 0x270ce20;
+    update_data.temp = 0xFFFF;
+    update_data.flags = 0;
+
+    ioctl(fb, MXCFB_SEND_UPDATE, &update_data);
+    return 0;
+}
+
 const nsfb_surface_rtns_t rm_rtns = {
     .defaults = rm_defaults,
     .initialise = rm_initialise,
     .finalise = rm_finalise,
     .input = rm_input,
+    .update = rm_update,
     .geometry = rm_set_geometry,
 };
 
