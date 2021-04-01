475621857bc9632bc6a805078aa6aee0da398959
Alex Friesenhahn <11029810+alex0809@users.noreply.github.com>
2021-04-01 16:36:27 +0200

    Import stdio in log.h, as it is used in the macro

--- src/surface/remarkable/log.h
+++ src/surface/remarkable/log.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define DEBUG_LOG(...) \
             do { if (DEBUG) { fprintf(stderr, "DEBUG: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); } } while (0)
 #define ERROR_LOG(...) \
